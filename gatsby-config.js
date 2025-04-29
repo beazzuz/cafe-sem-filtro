a794c580d7543711d1a59c4f5fff1ab64049219f
Isabela <147876367+beazzuz@users.noreply.github.com>
2025-04-29 15:08:30 -0300

    Terceira versão

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,9 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    pathPrefix: "/cafe-sem-filtro", 
+  siteMetadata: {
+    title: `Café Sem Filtro`,
   },
   plugins: [
      `gatsby-plugin-typescript`,
